ca35d4a99e698376375d69928f6e6b51ea136e23
alekseewatatianawork73 <153320481+alekseewatatianawork73@users.noreply.github.com>
2024-04-20 13:23:08 +0300

    Create logic.py

---
+++ logic.py
@@ -0,0 +1,12 @@
+def op_1(input_data):
+    number = int(input_data)
+    result_number = number * 4
+    output_data = str(result_number)
+    return output_data
+
+
+def op_2(input_data):
+    number = int(input_data)
+    result_number = number**2
+    output_data = str(result_number)
+    return output_data
